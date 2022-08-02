dfa61fe21776bdafb222bda791f17a4b904a779a
Juan D Chaves <juandavidchaves@protonmail.com>
2022-08-02 13:14:32 -0300

    added responsiveness

--- script.js
+++ script.js
@@ -2,13 +2,17 @@ const sendButton = document.getElementById("send-button");
 const inputField = document.getElementById("input-field");
 const email = document.getElementById("email");
 
+const re =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
 function sendEmail() {
-  let input = email.value;
-  if (input !== "") {
-    email.value = "";
-  } else if (input === "") {
+  if (email.value === "" || email.value.match(re) === null) {
     inputField.classList.add("error");
     email.classList.add("error");
+  } else if (email.value !== "") {
+    email.value = "";
+    inputField.classList.remove("error");
+    email.classList.remove("error");
   }
 }
 
